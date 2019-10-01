252f6d5cd91ac8cba1711d172b4bb86119916fc2
Vettree Labs <gobie@vettreelabs.com>
2019-10-01 11:45:06 -0400

    added js

---
+++ script.js
@@ -0,0 +1,14 @@
+//generate password
+function generate() {
+    let complexity = document.getElemenetById("slider").value
+    
+    let values = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567891234567890!@#$%^&*()_+";
+
+    let password = "";
+
+    for(var i = 0; i<= complexity; i++){
+        password = password + values.charAt(Math.floor(Math.random() * Math.floor(values.length - 1)));
+    }
+
+    document.getElementById("display").value = password;
+}
